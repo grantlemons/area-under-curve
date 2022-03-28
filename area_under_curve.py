418b64bd3e8e63eb18a70b51f5ed62b4bb6034bc
Grant Lemons <grant_lemons@yahoo.com>
2022-03-28 17:33:14 -0500

    made graphing optional

--- area_under_curve.py
+++ area_under_curve.py
@@ -3,17 +3,9 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 from matplotlib.patches import Polygon
 
-print('multiplication must have an asterisk (*) seperating factors')
-func = input('y = ')
-bnd_l = int(input('left bound: '))
-bnd_r = int(input('right bound: '))
-s_int = int(input ('sub-intervals: '))
+y = lambda x : eval(func, {__builtins__: {}, "np": np}, {"x": x})
 
-r_area = lambda x, y : abs(x * y)
-t_area = lambda x, y, z : abs(((x+y)/2)*z)
-y = lambda x : eval(parse_func(func), {__builtins__: {}}, {"x": x, "np": np})
-
-def parse_func(str):
+def compile_func(str):
     str = str.replace('ln', 'np.log')
     str = str.replace('^', '**')
     str = str.replace('sqrt', 'np.sqrt')
@@ -21,31 +13,29 @@ def parse_func(str):
     # try to remove the possibility of malicious code injection
     str = str.replace('_', '').replace('\"', '').replace('[', '').replace(']', '')
     str = str.strip()
-    return str
+    return compile(str, 'input', 'eval')
 
-def area_rectangles():
-    area = 0
+def area_rectangles(graph):
+    height = 0
+    width = (bnd_r-bnd_l)/s_int
     for i in range(0, s_int):
-        width = (bnd_r-bnd_l)/s_int
         mp = bnd_l+(width/2)+(width*i)
-        graph_rectangle(mp-(width/2), width, y(mp))
-        area += r_area(width, y(mp))
-    return area
+        height += abs(y(mp))
+        if graph: graph_rectangle(mp-(width/2), width, y(mp))
+    return height*width
 
-def area_trapezoids():
-    area = 0
+def area_trapezoids(graph):
+    height = 0
+    width = (bnd_r-bnd_l)/s_int
     for i in range(0, s_int):
-        width = (bnd_r-bnd_l)/s_int
-        l_x = bnd_l+(width*i)
-        r_x = l_x+width
-        graph_trapezoid(l_x, width, y)
-        area += t_area(y(l_x), y(r_x), width)
-    return area
+        height += abs((y(bnd_l+(width*i)) + y(bnd_l+(width*i+1)))/2)
+        if graph: graph_trapezoid(bnd_l+(width*i), width, y)
+    return height*width
 
 # graph functions
 fig, ax = plt.subplots()
 def graph_func():
-    nums = np.linspace(bnd_l, bnd_r, (bnd_r-bnd_l)*100)
+    nums = np.linspace(int(bnd_l), int(bnd_r), int((bnd_r-bnd_l)*100))
     ax.plot(nums, y(nums), color='blue')
     ax.plot([bnd_l, bnd_r], [0, 0], color='gray', alpha=1)
     
@@ -56,11 +46,18 @@ def graph_trapezoid(x, w, func):
     x_cords = [x, x+w, x+w      , x      ]
     y_cords = [0, 0  , func(x+w), func(x)]
     ax.add_patch(Polygon(xy=list(zip(x_cords,y_cords)), alpha=0.1, color='red'))
+    
+print('multiplication must have an asterisk (*) seperating factors')
+func = compile_func(input('y = '))
+bnd_l = float(input('left bound: '))
+bnd_r = float(input('right bound: '))
+s_int = int(input ('sub-intervals: '))
+graph = True if input('graph func (y/n): ').lower() == 'y' else False
 
-print('area with rectangles: {:0.3f}'.format(area_rectangles()))
-print('area with trapezoids: {:0.3f}'.format(area_trapezoids()))
+print('area with rectangles: {:0.3f}'.format(area_rectangles(graph)))
+print('area with trapezoids: {:0.3f}'.format(area_trapezoids(graph)))
 graph_func()
 try:
-    plt.show()
+    if graph: plt.show()
 except KeyboardInterrupt:
     exit()
